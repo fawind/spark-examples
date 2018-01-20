d5c7fbfd6eefd2ae8e5d3a8801c2e00f2204daae
fawind <windheuser.fabian@gmail.com>
2018-01-20 16:56:26 +0100

    Load datasets from directory or files

--- src/main/scala/AppConfig.scala
+++ src/main/scala/AppConfig.scala
@@ -1,14 +1,32 @@
-case class AppConfig (path: String = "", cores: Int = 1)
+import java.io.File
+
+case class AppConfig (cores: Int = 1, files: List[File] = List())
 
 object ArgParser {
   def parseConifg(args: Array[String]): AppConfig = {
     val parser = new scopt.OptionParser[AppConfig]("spark-uind") {
       opt[Int]("cores").action((cores, config) => config.copy(cores = cores))
-      opt[String]("path").action((path, config) => config.copy(path = path))
+      opt[String]("path").action((path, config) => {
+        val files = getFilesForDirectory(path)
+        config.copy(files = files)
+      })
+      opt[Seq[String]]("paths").action((paths, config) => {
+        val files = paths.toList.map(path => new File(path))
+        config.copy(files = files)
+      })
     }
     parser.parse(args, AppConfig()) match {
       case Some(config) => config
       case None => throw new IllegalArgumentException("No commandline args given")
+    }
+  }
+
+  private def getFilesForDirectory(directory: String): List[File] = {
+    val parentDir = new File(directory)
+    if (parentDir.exists && parentDir.isDirectory) {
+      parentDir.listFiles.filter(_.isFile).toList
+    } else {
+      throw new IllegalArgumentException("%s is not a directory" format directory)
     }
   }
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,22 +1,7 @@
-import java.io.File
-
-import org.apache.log4j.{Level, Logger}
-import org.apache.spark.sql.SparkSession
-
 object Main extends App {
 
   override def main(args: Array[String]): Unit = {
     val appConfig: AppConfig = ArgParser.parseConifg(args)
     println(appConfig)
-  }
-
-
-  def getFilesForDirectory(directory: String): List[File] = {
-    val parentDir = new File(directory)
-    if (parentDir.exists && parentDir.isDirectory) {
-        parentDir.listFiles.filter(_.isFile).toList
-    } else {
-      throw new IllegalArgumentException("%s is not a directory" format directory)
-    }
   }
 }
